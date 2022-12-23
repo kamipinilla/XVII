b0600210f5118ae10cc0a73830b1502e5d0d72d9
Kami <kamipinillar@gmail.com>
2022-12-22 21:24:05 -0500

    Update Brain.ts

--- serif/Brain.ts
+++ serif/Brain.ts
@@ -101,9 +101,6 @@ export default class Brain {
     const IMAGE_HEIGHT = 20;
     const IMAGE_CHANNELS = 1;  
     
-    // In the first layer of our convolutional neural network we have 
-    // to specify the input shape. Then we specify some parameters for 
-    // the convolution operation that takes place in this layer.
     model.add(layers.conv2d({
       inputShape: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
       kernelSize: 5,
@@ -113,12 +110,7 @@ export default class Brain {
       kernelInitializer: 'varianceScaling'
     }));
 
-    // The MaxPooling layer acts as a sort of downsampling using max values
-    // in a region instead of averaging.  
     model.add(layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
-    
-    // Repeat another conv2d + maxPooling stack. 
-    // Note that we have more filters in the convolution.
     
     model.add(layers.conv2d({
       kernelSize: 5,
@@ -129,13 +121,7 @@ export default class Brain {
     }));
     model.add(layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
     
-    // Now we flatten the output from the 2D filters into a 1D vector to prepare
-    // it for input into our last layer. This is common practice when feeding
-    // higher dimensional data to a final classification output layer.
     model.add(layers.flatten());
-
-    // Our last layer is a dense layer which has 10 output units, one for each
-    // output class (i.e. 0, 1, 2, 3, 4, 5, 6, 7, 8, 9).
 
     const NUM_OUTPUT_CLASSES = 40;
     model.add(layers.dense({
